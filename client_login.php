cf0bdb7623bc98f1e58ecb49861e82a56f624141
Kirandeep Singh <kirandeep@codenomad.net>
2015-05-24 17:39:02 +0530

    Initial Commit

---
+++ client_login.php
@@ -0,0 +1,202 @@
+<?php
+include_once 'header.php';
+
+include_once 'check_session.php';
+?>
+
+<!--div class="sec_create"> 
+
+    <h1> Create New Questionnaire </h1>
+</div>
+
+
+
+<div class="wrapper">
+
+    <form action="#" method="post">
+
+        <div class="center">
+
+            <div class="top_data">
+
+                <div class="left_data">
+
+                    <label>Questionnaire Name</label>
+
+
+
+                    <div class="qsn_name">
+
+                        <input type="text" class="input" name="ques">
+
+
+
+                    </div>
+
+                    <label>Client Login</label>
+
+
+
+                    <div class="name">
+
+                        <input type="text" class="input" name="clientname" placeholder="Username">
+
+                    </div>
+
+                    <div class="pass">
+
+                        <input type="text" class="input" name="password" placeholder="Password">
+
+                    </div>
+
+                </div>
+
+
+
+                <div class="right_data">
+
+                    <div class="run_time">
+
+                        <span>Questionnaire Run Time</span>
+
+                        <h2>11/7/15, 7:00am-8:00am <img src="images/minus.jpg"/></h2>
+
+                        <span> <img src="images/add.jpg"/> add time</span>
+
+                    </div>
+
+                    <div class="export">
+
+                        <span>Allow client to export?</span>
+
+                        <div class="onoffswitch">
+
+                            <input type="checkbox" name="onoffswitch" class="onoffswitch-checkbox" id="tab4" checked>
+
+                            <label class="onoffswitch-label" for="tab4">
+
+                                <span class="onoffswitch-inner"></span>
+
+                                <span class="onoffswitch-switch"></span>
+
+                            </label>
+
+                        </div>
+
+                    </div>
+
+                    <div class="import">
+
+                        <span>Allow client to import?</span>
+
+                        <div class="onoffswitch">
+
+                            <input type="checkbox" name="onoffswitch" class="onoffswitch-checkbox" id="tab5" checked>
+
+                            <label class="onoffswitch-label" for="tab5">
+
+                                <span class="onoffswitch-inner"></span>
+
+                                <span class="onoffswitch-switch"></span>
+
+                            </label>
+
+                        </div>
+
+                    </div>
+
+                </div>	 
+
+            </div>
+
+            <div class="clear"></div>
+
+            <div class="lastouter_sec"> 
+
+                <div class="button">
+
+                    <button type="button" class="btn" name="saveAndNext">Save & Next</button>
+
+                    <button type="button" class="btn" name="saveAndExit">Save & Exit</button>
+
+                    <button type="button" class="btn_cancel" name="cancel">Cancel</button>
+
+                </div>
+
+            </div>
+
+
+
+
+
+        </div>
+
+    </form>
+
+</div-->
+
+<div class="sec_create">
+    <h2 class="sec_head_ques"> Create a Questionnaire</h2>
+  </div>
+
+
+<div class="wrapper">
+       <div class="center">
+   <div class="top_data">
+      <div class="left_data">
+         <span>Questionnaire Name</span>
+		 <div class="qsn_name">
+		<input type="text" class="input">
+	      </div>
+	    <span>Client Login</span>
+		
+		     <div class="name">
+       		<input type="text" class="input" placeholder="Username">
+			</div>
+			<div class="pass">
+			<input type="text" class="input" placeholder="Password">
+			</div>
+	  </div>
+
+     <div class="right_data">
+         <div class="run_time">
+		   <span>Questionnaire Run Time</span>
+		   <h2>11/7/15, 7:00am-8:00am
+           <button class="bb_img"> <img src="images/minus.jpg"/></button></h2>
+		    <button class="bb_img"><img src="images/add.jpg"/></button>
+            <span> add time</span>
+		 </div>
+		 <div class="export">
+		   <span>Allow client to export?</span>
+		     <div class="onoffswitch">
+    <input type="checkbox" name="onoffswitch" class="onoffswitch-checkbox" id="tab4" checked>
+    <label class="onoffswitch-label" for="tab4">
+        <span class="onoffswitch-inner"></span>
+        <span class="onoffswitch-switch"></span>
+    </label>
+</div>
+		 </div>
+		 <div class="import">
+		 <span>Allow client to import?</span>
+		     <div class="onoffswitch">
+    <input type="checkbox" name="onoffswitch" class="onoffswitch-checkbox" id="tab5" checked>
+    <label class="onoffswitch-label" for="tab5">
+        <span class="onoffswitch-inner"></span>
+        <span class="onoffswitch-switch"></span>
+    </label>
+</div>
+		 </div>
+     </div>	 
+   </div>
+   <div class="clear"></div>
+   <div class="lastouter_sec">
+        <button class="butt_view bb_sec6"> Save&Next </button>
+        <button class="butt_view bb_sec6"> Save&Exit </button>
+        <button class="butt_view bb_sec6 bb_bg"> Cancel </button>
+      </div>
+</div>
+</div>
+
+</body>
+
+</html>
---
+++ header.php
@@ -0,0 +1,71 @@
+<?php
+session_start();
+
+function __autoload($classname) {
+    include "include/" . $classname . ".class.php";
+}
+
+?>
+<html>
+<head>
+<link rel="stylesheet" href="css/applogo.css">
+<script type="text/javascript" src="http://code.jquery.com/jquery-1.10.2.min.js"></script>
+<script type="text/javascript">
+            function validate()
+            {
+                var userObj = $("#username");
+                var passObj = $("#password");
+                flag = 1;
+                $("#user_error").html("");
+                $("#user_pass").html("");
+                if(userObj.val() === "") {
+                    $("#user_error").html('Please fill this field');
+                    flag = 0;
+                }
+                if(passObj.val() === "") {
+                    $("#user_pass").html('Please fill this field');
+                    flag = 0 ;
+                } 
+                
+                if(flag == 1){            
+                    $.ajax({
+                        method: "POST",
+                        url: "API/index.php",
+                        data: { username: userObj.val(), password: passObj.val(), type:"login" }
+                    }).done(function( data ) {
+                        var returnedData = JSON.parse(data);
+                       // alert(returnedData);
+                        if(returnedData.code == 1) {
+                            location.href = "admin.php";
+                        } else {
+                            alert(returnedData.message);
+                        }
+                      });
+                }
+                  
+                  return false;
+            }
+            
+        </script>
+</head>
+<body>
+<header>
+  <div class="wrapper">
+    <div class="outer_header">
+      <div class="logo head"> <img src="images/logo.jpg" alt="logo"> </div>
+      <div class="ryt_admin">
+        <div class="inner_admin">
+          <div class="droper">
+            <div class="dropper_inner">
+              <div class="user_name"> Admin <img src="images/arrow.jpg" alt="arrow" id="show"> </div>
+              <div class="htab_o">
+                <div class="htab hr_l"> Change Password </div>
+                <div class="htab"> Logout </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+	</div>
+	</header>
